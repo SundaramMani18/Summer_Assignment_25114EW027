9e865293118251cf58063ba1f82889d022410d44
SundaramMani18 <sundarammani1804@gmail.com>
2026-06-24 12:49:07 +0530

    Add Day-16 tasks

---
+++ Day-16/Q63.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a[100], n, i, j, sum;
+    printf("Enter size of array:" );
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(i=0;i<n;i++){
+    scanf("%d", &a[i]);
+}
+    printf("Enter required sum are : ");
+    scanf("%d",&sum);
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]+a[j]==sum){
+                printf("Pair=%d and %d",a[i],a[j]);
+            }
+        }
+    }
+    return 0;
+}
